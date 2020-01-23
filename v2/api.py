a2659094109117027dd1605343b35f08c1e336b2
TheNovi <jakub.novi.novacek@gmail.com>
2020-01-23 18:18:40 +0100

    Improve Api.quick_run() (also rename from run()) A RetCom.com type hint

--- v2/api.py
+++ v2/api.py
@@ -1,6 +1,8 @@
 from typing import List, Union
 
 from .com import Com
+from .consts import RetCode
+from .retcom import RetCom
 from .shell import Shell
 
 
@@ -13,7 +15,20 @@ class Api:
 		# Overwrite me
 		return []
 
-	def run(self):  # TODO Maybe run one command
+	def quick_run(self, com: str):
 		if not self.shell:
 			self.shell = Shell(self)
-		print("Running TODO")
+		rc: RetCom
+		for rc in self.shell.line_com_parser(com):
+			if rc.code == RetCode.EXIT:
+				return False
+			elif rc.code == RetCode.ERROR:
+				print(f"[Error] {rc.com.name}{(': ' + rc.answer) if rc.answer else ''}")
+			elif rc.code == RetCode.PATH_ERROR:
+				print(f"[Path Error] {rc.com.name}: {rc.answer}")
+			elif rc.code == RetCode.ARGS_ERROR:
+				print(f"[Args Error] {rc.com.name}{(': ' + rc.answer) if rc.answer else ''}")
+			elif rc.code == RetCode.UNKNOWN:
+				print(f"[Unknown command] {rc.com.name}")
+			elif rc.answer:
+				print(rc.answer)
--- v2/retcom.py
+++ v2/retcom.py
@@ -1,12 +1,13 @@
 from typing import List
 
+from .com import Com
 from .consts import RetCode
 
 
 class RetCom:
 	def __init__(self, shell, com, args: List[str]):
 		self.shell = shell
-		self.com = com
+		self.com: Com = com
 		self.args: List[str] = args
 		self.code: RetCode = RetCode.NONE
 		self.answer: str = ''
