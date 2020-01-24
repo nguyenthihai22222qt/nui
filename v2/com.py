70b41c95215852d5b972aec15e7c6e287c4a9e97
TheNovi <jakub.novi.novacek@gmail.com>
2020-01-25 00:00:01 +0100

    Make constructor parameter in Com 'name' accept str

--- v2/com.py
+++ v2/com.py
@@ -1,8 +1,8 @@
-from typing import List
+from typing import List, Union
 
 
 class Com:
-	def __init__(self, name: List[str], method: callable, man: str = ''):  # TODO name str
-		self.name: List[str] = name
+	def __init__(self, name: Union[str, List[str]], method: callable, man: str = ''):
+		self.name: List[str] = name if isinstance(name, List) else name.strip().split(' ')
 		self.method: callable = method
 		self.man = man  # TODO Better help
