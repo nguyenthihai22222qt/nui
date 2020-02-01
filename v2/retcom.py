2902fb31081d99ebe08d43860f439becc7fd7198
TheNovi <jakub.novi.novacek@gmail.com>
2020-02-01 16:49:37 +0100

    RetCom.get_arg(index, default, method)

--- v2/retcom.py
+++ v2/retcom.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Callable
 
 from .com import Com
 from .consts import RetCode
@@ -12,19 +12,26 @@ class RetCom:
 		self.code: RetCode = RetCode.NONE
 		self.answer: str = ''
 
+	def get_arg(self, index: int, default=None, method: Callable = str):
+		# noinspection PyBroadException
+		try:
+			return method(self.args[index])
+		except:
+			return default
+
 	def quick(self, answer: str = '', code: RetCode = RetCode.OK) -> 'RetCom':
 		self.answer = answer
 		self.code = code
 		return self
 
-	def unknown(self) -> 'RetCom':
-		self.answer = self.args[0] if len(self.args) else ''
-		self.code = RetCode.UNKNOWN
-		return self
-
 	def error(self, answer: str = '') -> 'RetCom':
 		self.answer = answer
 		self.code = RetCode.ERROR
+		return self
+
+	def unknown(self) -> 'RetCom':
+		self.answer = self.args[0] if len(self.args) else ''
+		self.code = RetCode.UNKNOWN
 		return self
 
 	def empty(self):
