6634683117fbcf43fc09338d7a876118e8e450a4
TheNovi <jakub.novi.novacek@gmail.com>
2020-02-16 18:01:54 +0100

    Basic Stage/Scene fun

--- v1/__init__.py
+++ v1/__init__.py
@@ -1,14 +1,19 @@
 import tkinter
 
+from .style import Style
 from .widgets import *
 
 
 class Stage(tkinter.Frame):
-	def __init__(self):
-		super().__init__()
+	def __init__(self, style: Style):
+		super().__init__(tkinter.Tk(), bg=style.bg)
+		self.style = style
+
+	def run(self):
+		self.master.mainloop()
 
 
 class Scene(tkinter.Frame):
-	def __init__(self, stage):
-		super().__init__(stage)
-		self.stage = stage
+	def __init__(self, stage: Stage):
+		super().__init__(stage, bg=stage.style.bg)
+		self.stage: stage = stage
