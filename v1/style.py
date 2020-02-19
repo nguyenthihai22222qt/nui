5d504b7f1203316b76a988e5a7e3427e0eddcb5a
TheNovi <jakub.novi.novacek@gmail.com>
2020-02-19 19:13:12 +0100

    - _widget.py - Style.quick() (basically deleted last commit, but improved import path to style.py :))

--- v1/style.py
+++ v1/style.py
@@ -2,11 +2,3 @@ class Style:
 	def __init__(self, bg: str = 'white', fg: str = 'black'):
 		self.bg = bg
 		self.fg = fg
-
-	def quick(self, bg=False, fg=False):
-		o = {}
-		if bg:
-			o['bg'] = self.bg
-		if fg:
-			o['fg'] = self.fg
-		return o
--- v1/widgets/label.py
+++ v1/widgets/label.py
@@ -1,10 +1,9 @@
 from tkinter import Label as tkLabel
 
-from v1._widget import _Widget
-from v1.style import Style
+from ..style import Style
 
 
-class Label(_Widget, tkLabel):
-	def __init__(self, master, style: Style, **kw):
-		tkLabel.__init__(self, master, **style.quick(bg=True, fg=True), **kw)
-		_Widget.__init__(self, style)
+class Label(tkLabel):
+	def __init__(self, master, style: 'Style', **kw):
+		super().__init__(master, bg=style.bg, fg=style.fg, **kw)
+		self.style = style
