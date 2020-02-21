02fc391da30d3254300c872938921a024d610f3a
TheNovi <jakub.novi.novacek@gmail.com>
2020-02-21 19:54:14 +0100

    + Entry

--- v1/widgets/basic.py
+++ v1/widgets/basic.py
@@ -14,3 +14,9 @@ class Button(tkinter.Button):
 	def __init__(self, master, style: 'Style', command: Callable, **kw):
 		super().__init__(master, bg=style.bg, fg=style.fg, command=command, **kw)
 		self.style = style
+
+
+class Entry(tkinter.Entry):
+	def __init__(self, master, style: 'Style', **kw):
+		super().__init__(master, bg=style.bg, fg=style.fg, insertbackground=style.fg, **kw)
+		self.style = style
