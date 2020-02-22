af30f5b34e25940414fda2d686e01e15f711c8f3
TheNovi <jakub.novi.novacek@gmail.com>
2020-02-22 02:02:33 +0100

    Basic Stage/Scene fun Scenes are now stored inside Stage._scenes: Dict[str, 'Scene']

--- v1/__init__.py
+++ v1/__init__.py
@@ -1,3 +1,5 @@
+from typing import Union, Dict, Type
+
 from .style import Style
 from .widgets import *
 
@@ -6,8 +8,34 @@ class Stage(tkinter.Frame):
 	def __init__(self, style: Style):
 		super().__init__(tkinter.Tk(), bg=style.bg)
 		self.style = style
+		self._active: Union['Scene', type] = type("TempScene", (), {'deactivate': lambda: None})
+		self._scenes: Dict[str, 'Scene'] = {}
 
-	def run(self):
+		self.master.bind('<Key>', self.typed)
+
+	def add(self, name: str, scene: Type['Scene']):
+		self._scenes[name] = scene(self)
+		return self
+
+	def __getitem__(self, item: str):
+		return self._scenes.get(item)
+
+	def typed(self, event):
+		self._active.typed(event)
+
+	def switch(self, to: 'Scene'):
+		self._active.deactivate()
+		self._active = to
+		self._active.activate()
+		self._active.focus_set()
+
+	def tick(self):
+		self._active.tick()
+		self.after(10, self.tick)
+
+	def run(self, scene: Union['Scene', str]):
+		(scene if isinstance(scene, Scene) else self._scenes.get(scene)).switch_to_me()
+		self.master.after(1, self.tick)
 		self.master.mainloop()
 
 
@@ -15,3 +43,29 @@ class Scene(tkinter.Frame):
 	def __init__(self, stage: Stage):
 		super().__init__(stage, bg=stage.style.bg)
 		self.stage: stage = stage
+
+	def tick(self) -> None:
+		"""
+		Overwrite me
+		This is called every 10 ticks (Number could change).
+		:return: None
+		"""
+		pass
+
+	def typed(self, event) -> None:
+		"""
+		Overwrite me
+		Fired every time user preses key while window focused.
+		:param event: tkinter bind <Ket> event
+		:return: None
+		"""
+		pass
+
+	def activate(self):
+		self.pack(fill='both', expand=True)
+
+	def deactivate(self):
+		self.pack_forget()
+
+	def switch_to_me(self):
+		self.stage.switch(self)
