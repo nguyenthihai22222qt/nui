a50ce3cff6f44944fd7f4f7fa6b5d0d97f47c89a
TheNovi <jakub.novi.novacek@gmail.com>
2020-02-29 03:32:22 +0100

    Fix #7

--- v1/widgets/arch.py
+++ v1/widgets/arch.py
@@ -19,3 +19,23 @@ class Field(Frame):
 		super().__init__(master, **kw)
 		self.label = Label(self, text=label)
 		self.label.pack(side='left')
+		self.b_get = lambda: None
+		self.b_set = lambda v: None
+		self.auto_write: bool = False
+
+	def value_change_event(self, _=None):  # TODO Test how many times this calls write/read
+		if self.auto_write:
+			self.write()
+			self.read()
+
+	def write(self):
+		self.b_set(self.get())
+
+	def read(self):
+		self.set_(self.b_get())
+
+	def get(self):
+		raise Exception(f"Get method of field f{self.__class__}: {self.label['text']}")
+
+	def set_(self, value):
+		raise Exception(f"Get method of field f{self.__class__}: {self.label['text']}")
--- v1/widgets/form.py
+++ v1/widgets/form.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Type, Callable
 
 from .arch import Frame, Field
 from .basic import Entry, Text
@@ -9,21 +9,52 @@ class Form(Frame):
 		super().__init__(master, **kw)
 		self._fields = []
 
-	def add_field(self, field: Type[Field], label: str, **kw):
-		self._fields.append(field(self, label, **kw))
-		self._fields[-1].pack(fill='x', expand=True)
+	def add_field(self, field: Type[Field], label: str, bind_get: Callable[[], str] = lambda: None, bind_set: Callable[[str], None] = lambda v: None, /, *, auto_write: bool = False, **kw):
+		f = field(self, label, **kw)
+		f.b_get = bind_get
+		f.b_set = bind_set
+		f.auto_write = auto_write
+		self._fields.append(f)
+		self._fields[-1].pack(fill='x')
 		return self
+
+	def set_fields(self):
+		for field in self._fields:
+			field.read()
+		return self
+
+	def write_fields(self):
+		for field in self._fields:
+			field.write()
 
 
 class EntryField(Field):
 	def __init__(self, master, label: str, **kw):
 		super().__init__(master, label, **kw)
 		self.input = Entry(self, width=1)
+		self.input.bind('<KeyRelease>', super().value_change_event)  # TODO Move to Field
 		self.input.pack(side='right', fill='x', expand=True)
+
+	def get(self):
+		return self.input.get()
+
+	def set_(self, value):
+		value = value if value else ''
+		self.input.delete(0, 'end')
+		self.input.insert('end', value)
 
 
 class TextField(Field):
 	def __init__(self, master, label: str, **kw):
 		super().__init__(master, label, **kw)
 		self.input = Text(self, width=1, height=2)
+		self.input.bind('<KeyRelease>', super().value_change_event)
 		self.input.pack(side='right', fill='x', expand=True)
+
+	def get(self):
+		return self.input.get(1.0, 'end').strip()
+
+	def set_(self, value):
+		value = value if value else ''
+		self.input.delete(1.0, 'end')
+		self.input.insert('end', value)
