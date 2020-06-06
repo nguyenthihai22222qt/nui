1d2939a22582ee2b74293c1ff0ec09e79e795be1
TheNovi <jakub.novi.novacek@gmail.com>
2020-06-06 17:51:30 +0200

    Add text arg to labels and buttons. Closes #32

--- nui/gui/v1/widgets/basic.py
+++ nui/gui/v1/widgets/basic.py
@@ -5,15 +5,15 @@ from .imethods import IMethods
 
 
 class Label(tkinter.Label, IMethods):
-	def __init__(self, master, **kw):
+	def __init__(self, master, text: str = '', **kw):
 		self.stage = master.stage
-		super().__init__(master, bg=self.stage.style.bg, fg=self.stage.style.fg, **kw)
+		super().__init__(master, text=text, bg=self.stage.style.bg, fg=self.stage.style.fg, **kw)
 
 
 class Button(tkinter.Button, IMethods):
-	def __init__(self, master, command: Callable, **kw):
+	def __init__(self, master, text: str, command: Callable, **kw):
 		self.stage = master.stage
-		super().__init__(master, bg=self.stage.style.bg, fg=self.stage.style.fg, command=command, **kw)
+		super().__init__(master, text=text, bg=self.stage.style.bg, fg=self.stage.style.fg, command=command, **kw)
 
 
 class Entry(tkinter.Entry, IMethods):
--- nui/gui/v1/widgets/flat_button.py
+++ nui/gui/v1/widgets/flat_button.py
@@ -2,13 +2,11 @@ import tkinter
 from inspect import getfullargspec
 from typing import Callable
 
+from .imethods import IMethods
 
-class FlatButton(tkinter.Label):
-	def __init__(self, master, command: Callable, **kw):
+
+class FlatButton(tkinter.Label, IMethods):
+	def __init__(self, master, text: str, command: Callable, **kw):
 		self.stage = master.stage
-		super().__init__(master, bg=self.stage.style.bg, fg=self.stage.style.fg, **kw)
+		super().__init__(master, text=text, bg=self.stage.style.bg, fg=self.stage.style.fg, **kw)
 		self.bind('<Button-1>', command if getfullargspec(command).args else lambda _: command())
-
-	def inline_pack(self, /, *, fill='both', side='top', expand=False, **kw) -> 'FlatButton':
-		self.pack(fill=fill, side=side, expand=expand, **kw)
-		return self
