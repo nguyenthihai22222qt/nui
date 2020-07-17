20abe876e5d955b99fcd60eccb1e241781f47971
TheNovi <jakub.novi.novacek@gmail.com>
2020-07-18 01:14:39 +0200

    Load/Save from/to dict/json format #49

--- nui/gui/v1/style.py
+++ nui/gui/v1/style.py
@@ -3,6 +3,17 @@ from typing import Tuple
 
 class Style:
 	def __init__(self, /, *, bg: str = 'white', fg: str = 'black', font: Tuple[str, int] = None):
-		self.bg = bg
-		self.fg = fg
-		self.font = font
+		self.bg = bg if bg else None
+		self.fg = fg if fg else None
+		self.font = font if font else None
+
+	@staticmethod
+	def from_dict(in_: dict) -> 'Style':
+		return Style(bg=in_.get('bg'), fg=in_.get('fg'), font=in_.get('font'))
+
+	def to_dict(self) -> dict:
+		return {
+			'fg': self.fg,
+			'bg': self.bg,
+			'font': self.font
+		}
