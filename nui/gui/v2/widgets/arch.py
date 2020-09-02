53f9417416207494440d6ec6317d941d60f928b6
TheNovi <jakub.novi.novacek@gmail.com>
2020-09-02 16:51:35 +0200

    + Frame.pack_kw_helper() (updates #56)

--- nui/gui/v2/widgets/arch.py
+++ nui/gui/v2/widgets/arch.py
@@ -1,5 +1,5 @@
 import tkinter
-from typing import Type
+from typing import Type, Dict
 
 from .basic import IMethods
 from .. import Style
@@ -20,7 +20,7 @@ class Frame(tkinter.Frame, IMethods):
 		:return: self
 		"""
 		widget_kw = widget_kw if widget_kw else {}
-		pack_kw = pack_kw if pack_kw else {'fill': 'both'}
+		pack_kw = pack_kw if pack_kw else Frame.pack_kw_helper()
 		widget(self, **widget_kw).pack(**pack_kw)
 		return self
 
@@ -37,6 +37,19 @@ class Frame(tkinter.Frame, IMethods):
 		o = widget(self, **widget_kw)
 		o.pack(**pack_kw)  # tkinter doesn't have inline_pack()
 		return o
+
+	@staticmethod
+	def pack_kw_helper(*, fill='both', side='top', anchor='c', expand=False, **kw) -> Dict:
+		"""
+		Little help with .pack() arguments. Mostly used in Frame.add(pack_kw=Frame.pack_kw_helper()), but can be used in normal pack methods: widget.pack(**Frame.pack_kw_helper())\n
+		:param fill: Specifies whether the widget should occupy all the space provided to it by the master. If NONE, keep the widget’s original size. If X (fill horizontally), Y (fill vertically), or BOTH (default), fill the given space along that direction. To make a widget fill the entire master widget, set fill to BOTH and expand to a non-zero value.
+		:param side: Specifies which side to pack the widget against. To pack widgets vertically, use TOP (default). To pack widgets horizontally, use LEFT. You can also pack widgets along the BOTTOM and RIGHT edges.
+		:param anchor: Anchor must be a valid anchor position such as n or sw; it specifies where to position each slave in its parcel. Defaults to center.
+		:param expand: Specifies whether the widgets should be expanded to fill any extra space in the geometry master. If false (default), the widget is not expanded.
+		:param kw: Other options (see official tkinter.Widget.pack() documentation)
+		:return: arguments in form of Dict
+		"""
+		return {'fill': fill, "side": side, "anchor": anchor, "expand": expand, **kw}
 
 
 class PopUp(tkinter.Frame, IMethods):
