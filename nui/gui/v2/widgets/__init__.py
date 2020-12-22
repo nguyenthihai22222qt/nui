7588d7b55f5854c77e07bcbfd4eaba2feec1264c
TheNovi <jakub.novi.novacek@gmail.com>
2020-12-22 21:58:16 +0100

    Closes #63 Basic Date field

--- nui/gui/v2/widgets/__init__.py
+++ nui/gui/v2/widgets/__init__.py
@@ -1,6 +1,7 @@
 from .arch import Frame, PopUp
 from .basic import Label, Button, Entry, Text, Listbox
 from .button_groups import CheckBoxButtonGroup, RadioButtonGroup
+from .date import Date
 from .flat_button import FlatButton
 from .form import Form
 from .search import Search, SearchPopUp, SearchButton
