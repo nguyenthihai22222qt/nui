f770d8e1be8fa4659a4467abec5b9780ca2e6238
TheNovi <jakub.novi.novacek@gmail.com>
2021-02-15 23:40:35 +0100

    Upload to PyPI

--- setup.py
+++ setup.py
@@ -1,21 +1,5 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-	name='nui',
-	version='g2.17-c3.1',  # <name (g/c)><newest 'folder' ver>-<same format, next part>
-	url='https://github.com/TheNovi/Nui',
-	license='MIT',
-	author='TheNovi',
-	author_email='jakub.novi.novacek@gmail.com',
-	description='',
-	packages=find_packages(),
-	classifiers=[
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Developers',
-		'Topic :: Software Development :: Ui Tools',
-		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python :: 3.9',
-	],
-)
+setuptools.setup()
 
 # pip install .
